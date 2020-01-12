a3c50a42e36c312f4fd82bcb0fcba5552cea8055
Kastriot Salihu <49651929+KastriotSalihu@users.noreply.github.com>
2020-01-12 19:53:24 +0100

    Update RekurzioneFile.cpp

--- RekurzioneFile.cpp
+++ RekurzioneFile.cpp
@@ -2,138 +2,56 @@
 #include<string>
 #include <algorithm>
 #define min(x,y) ((x<y)?x:y)
-
-//		   -----------------------------------------------------------------------------------------------
-//Factorial			--------------------------------------------------------------------------------------
-//							  ----------------------------------------------------------------------------
-
-int factorial_b(int n);
-
+	   
+int factorial_b(int n);																						   
 //Finds the factorial of a given number n (n!)
-int faktorial(int n)
-{
-	if (n > 1)
-		return n * faktorial(n - 1);
-	else
-		return 1;
-}
-
 int factorial_a(int n) {
-		if (n <= 1)
-			return 1;
-		else
-			return n * factorial_b(n - 1);
-	}
-
+	if (n <= 1)   return 1;
+	else  
+		return n * factorial_b(n - 1);
+}			   
 int factorial_b(int n) {
-		if (n <= 1)
-			return 1;
-		else
-			return n * factorial_a(n - 1);
-}
-
-//							 ----------------------------------------------------------------------------------
-//Factorial		     ------------------------------------------------------------------------------------------
-//			---------------------------------------------------------------------------------------------------
-
-
+	if (n <= 1)   return 1;
+	else  
+		return n * factorial_a(n - 1);
+}			   					
 // Prints n n-1 ... 2 1 1 2 ...	n-1 n
-void Numerim(int test)
-{
-	if (test < 1)
-		return;
-	else
-	{
+void Numerim(int test){
+	if (test < 1)   return;
+	else{
 		std::cout << test << " ";
 		Numerim(test - 1);    // statement 2
 		std::cout << test << " ";
 		return;
 	}
-}
-
+}										 
 //Displays integers from n to 0
 void display(int n) {
-		if (n < 1)
-			return;
-		std::cout << n <<"\t" ;
-		display(n - 1);
-}
-
+	if (n < 1)
+		return;
+	std::cout << n << "\t";
+	display(n - 1);
+}									   
 // Recursive function to print the pattern without any extra variable
-void printPattern(int n, int m)
-{
-	// Base case (When n becomes 0 or negative)
-	if (n == 0 || n < 0)
-	{
+void printPattern(int n, int m)	{
+	if (n == 0 || n < 0){
 		std::cout << n << " ";
 		return;
-	}
-
+	} 
 	// First print decreasing order
 	std::cout << n << " ";
-	printPattern(n - m,m);
-
+	printPattern(n - m, m);		
 	// Then print increasing order
 	std::cout << n << " ";
-}
-
-
-//		   -----------------------------------------------------------------------------------------------
-//Fibonacci			--------------------------------------------------------------------------------------
-//							  ----------------------------------------------------------------------------
-
-//Display Fibonacci up to n
-void fibSeries(int n) {
-	static int n1 = 0, n2 = 1, n3;
-	if (n > 0) {
-		n3 = n1 + n2;
-		n1 = n2;
-		n2 = n3;
-		std::cout << n3 << " ";
-		fibSeries(n - 1);
-	}
-}
-
-//returns n-th member in the Fibonacci sequence
-int Fibonacci(int n) {
-	if (n <= 2)
-		return 1;
-	else
-		return Fibonacci(n - 1) + Fibonacci(n - 2);
-}
-
-//returns n-th member in the Fibonacci sequence	1 2 3 5 8
-int Fibonacci0(int n) {
-	if (n <= 1)
-		return 1;
-	else
-		return Fibonacci(n - 1) + Fibonacci(n - 2);
-}
-
-//							 ----------------------------------------------------------------------------------
-//Fibonacci			-------------------------------------------------------------------------------------------
-//			---------------------------------------------------------------------------------------------------
-
-
-
-
-
-
-//		   -----------------------------------------------------------------------------------------------
-//Palindrome         -------------------------------------------------------------------------------------
-//							  ----------------------------------------------------------------------------
-
-//Reverses order of digits
-int reverse_digits(int n, int temp)
-{
+}											
+ //Reverses order of digits
+int reverse_digits(int n, int temp){
 	if (n == 0)
-		return temp;
-
+		return temp; 
 	temp = (temp * 10) + (n % 10);
-	return reverse_digits(n / 10, temp);
-
-}
-
+	return reverse_digits(n / 10, temp);   
+}				
+//Checks whether number is palindrome
 bool checkPalindrome(int num) {
 	int result = reverse_digits(num, 0);
 	if (result == num) {
@@ -143,20 +61,11 @@ bool checkPalindrome(int num) {
 	else {
 		std::cout << "Numri " << num << " nuk eshte palindrom" << std::endl;
 		return 0;
-	}
-
-}
-
-//							 ----------------------------------------------------------------------------------
-//Palindrome        -------------------------------------------------------------------------------------------
-//			---------------------------------------------------------------------------------------------------
-
-
-// is Prime or Not using Recursion
-// Returns true if n is prime, else
-// return false.
-// i is current divisor to check.
-bool isPrime(int n, int i = 2){
+	}		 
+}								 
+// is Prime or Not using Recursion Returns true if n is prime, else
+// return false. i is current divisor to check.
+bool isPrime(int n, int i = 2) {
 	// Base cases
 	if (n <= 2)
 		return (n == 2) ? true : false;
@@ -168,13 +77,13 @@ bool isPrime(int n, int i = 2){
 	return isPrime(n, i + 1);
 }
 // Function to check sum of digit
-int sum_of_digit(int n){
+int sum_of_digit(int n) {
 	if (n == 0)
 		return 0;
 	return (n % 10 + sum_of_digit(n / 10));
 }
 // recursive function to calculate multiplication of two numbers
-int product(int x, int y){
+int product(int x, int y) {
 	// if x is less than
 	// y swap the numbers
 	if (x < y)
@@ -185,75 +94,9 @@ int product(int x, int y){
 	// iteratively calculate y times sum of x
 	else
 		return (x + product(x, y - 1));
-}
-//
-//// Prints first n terms of Recaman sequence
-//void recaman(const int no)
-//{
-//	const int n = no;
-//	// Create an array to store terms
-//	int arr[n];
-//
-//	// First term of the sequence is always 0
-//	arr[0] = 0;
-//	printf("%d, ", arr[0]);
-//
-//	// Fill remaining terms using recursive
-//	// formula.
-//	for (int i = 1; i < n; i++)
-//	{
-//		int curr = arr[i - 1] - i;
-//		int j;
-//		for (j = 0; j < i; j++)
-//		{
-//			// If arr[i-1] - i is negative or
-//			// already exists.
-//			if ((arr[j] == curr) || curr < 0)
-//			{
-//				curr = arr[i - 1] + i;
-//				break;
-//			}
-//		}
-//
-//		arr[i] = curr;
-//		printf("%d, ", arr[i]);
-//	}
-//}
-
-
-//// Prints first n terms of Recaman sequence
-//void recaman(int n)
-//{
-//	if (n <= 0)
-//		return;
-//
-//	// Print first term and store it in a hash
-//	printf("%d, ", 0);
-//	unordered_set<int> s;
-//	s.insert(0);
-//
-//	// Print remaining terms using recursive
-//	// formula.
-//	int prev = 0;
-//	for (int i = 1; i < n; i++)
-//	{
-//		int curr = prev - i;
-//
-//		// If arr[i-1] - i is negative or
-//		// already exists.
-//		if (curr < 0 || s.find(curr) != s.end())
-//			curr = prev + i;
-//
-//		s.insert(curr);
-//
-//		printf("%d, ", curr);
-//		prev = curr;
-//	}
-//}
-
-// Recursive function to sort an array using
-// insertion sort
-void insertionSortRecursive(int arr[], int n){
+}	 	
+// Recursive function to sort an array using insertion sort
+void insertionSortRecursive(int arr[], int n) {
 	// Base case
 	if (n <= 1)
 		return;
@@ -266,33 +109,31 @@ void insertionSortRecursive(int arr[], int n){
 	/* Move elements of arr[0..i-1], that are
 	  greater than key, to one position ahead
 	  of their current position */
-	while (j >= 0 && arr[j] > last){
+	while (j >= 0 && arr[j] > last) {
 		arr[j + 1] = arr[j];
 		j--;
 	}
 	arr[j + 1] = last;
 }
 // Returns n'th term in look-and-say sequence
-std::string lookAndSay(int n){
+std::string lookAndSay(int n) {
 	// Base cases
 	if (n == 1)      return "1";
 	if (n == 2)      return "11";
 	// Find n'th term by generating all terms from 3 to
 	// n-1.  Every term is generated using previous term
 	std::string str = "11"; // Initialize previous term
-	for (int i = 3; i <= n; i++){
-		// In below for loop, previous character
-		// is processed in current iteration. That
-		// is why a dummy character is added to make
-		// sure that loop runs one extra iteration.
+	for (int i = 3; i <= n; i++) {
+		// In below for loop, previous character is processed in current iteration. That
+		// is why a dummy character is added to make sure that loop runs one extra iteration.
 		str += '$';
 		int len = str.length();
 		int cnt = 1; // Initialize count of matching chars
 		std::string  tmp = ""; // Initialize i'th term in series
 		// Process previous term to find the next term
-		for (int j = 1; j < len; j++){
+		for (int j = 1; j < len; j++) {
 			// If current character does't match
-			if (str[j] != str[j - 1]){
+			if (str[j] != str[j - 1]) {
 				// Append count of str[j-1] to temp
 				tmp += cnt + '0';
 				// Append str[j-1]
@@ -300,8 +141,7 @@ std::string lookAndSay(int n){
 				// Reset count
 				cnt = 1;
 			}
-			//  If matches, then increment count of matching
-			// characters
+			//  If matches, then increment count of matching characters
 			else cnt++;
 		}
 		// Update str
@@ -309,51 +149,41 @@ std::string lookAndSay(int n){
 	}
 	return str;
 }
-// Function to copy one string in to other using recursion
-void myCopy(char s1[], char s2[], int index = 0){
-	// copying each character from s1 to s2
-	s2[index] = s1[index];
-	// if string reach to end then stop
-	if (s1[index] == '\0')
-		return;
-	// increase character index by one
-	myCopy(s1, s2, index + 1);
-}
 // Function to check for consonant
-bool isConsonant(char ch){
+bool isConsonant(char ch) {
 	// To handle lower case
 	ch = toupper(ch);
 	return !(ch == 'A' || ch == 'E' ||
 		ch == 'I' || ch == 'O' ||
 		ch == 'U') && ch >= 65 && ch <= 90;
 }
+//Removes Spaces
+int removeSpaces(char* str,int n) {
+	int cnt = 0;
+	for (int i = 0; i < n;i++) {
+		if (str[i] == ' ') {
+			str[i] = 'a';
+			cnt++;
+		}
+	}
+	return cnt;
+}								
 // to count total number of consonants from 0 to n-1
-int totalConsonants(std::string str, int n)
+int totalConsonants(char *str, int n)
 {
 	if (n == 1)
-		return isConsonant(str[0]);
-
+		return isConsonant(str[0]);			 
 	return totalConsonants(str, n - 1) + isConsonant(str[n - 1]);
 }
 // CPP program to find minimum (or maximum) element in an array.
-int getMin(int arr[], int n)
-{
-	// If there is single element, return it.
-	// Else return minimum of first element and
-	// minimum of remaining array.
-	return (n == 1) ? arr[0] :min(arr[0], getMin(arr + 1, n - 1));
+int getMin(int arr[], int n){ 
+	return (n == 1) ? arr[0] : min(arr[0], getMin(arr + 1, n - 1));
 }
-int getMax(int arr[], int n)
-{
-	// If there is single element, return it.
-	// Else return maximum of first element and
-	// maximum of remaining array.
+int getMax(int arr[], int n) {
 	return (n == 1) ? arr[0] : std::max(arr[0], getMax(arr + 1, n - 1));
 }
 int sum(int n) {
 	if (n != 0)
-		// sum() function calls itself
 		return n + sum(n - 1);
-	else
-		return n;
+	else   return n;
 }
