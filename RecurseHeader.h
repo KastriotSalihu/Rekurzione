769c8cf25d188ebfa730335592058a52d2b0f6a9
Kastriot Salihu <49651929+KastriotSalihu@users.noreply.github.com>
2020-01-12 19:46:36 +0100

    Update RecurseHeader.h

--- RecurseHeader.h
+++ RecurseHeader.h
@@ -2,40 +2,23 @@
 #define newl std::cout<<std::endl;	//Prints a new line
 #define spac std::cout<<" "	    //Prints a space
 #define LOG() std::cout			 //Prints
-//#define shtyp() std::cout
-int faktorial(int n);
-int factorial_a(int n);
 
-void printArray(int arr[], int n);
+int factorial_a(int n);												  //Used	 
+void Numerim(int test);												  //Used
+void display(int n);												  //Used
+void printPattern(int n, int m);									  //Used			 
+int reverse_digits(int n, int temp);								  //Used as Secondary to P
+bool checkPalindrome(int numeri);									  //Used P
+bool isPrime(int n, int i = 2);								  		  //Used
+int sum_of_digit(int n);									 		  //Used
+int product(int x, int y);											  //Used
+void insertionSortRecursive(int arr[], int n);			 			  //Used
+std::string lookAndSay(int n);									 	  //Used
+bool isConsonant(char ch);											  //Used as Secondary to J
+int removeSpaces(char* str, int n);									  //Used as Secondary to J
+int totalConsonants(char *str, int n);								  //Used J
+int getMax(int arr[], int n);										  //Used K
+int getMin(int arr[], int n);										  //Used K
+int sum(int n);														  //Used 
 
-void Numerim(int test);
-void display(int n);
-void printPattern(int n, int m);										   //6
-
-int Fibonacci(int n);
-int Fibonacci0(int n);
-void fibSeries(int n);
-
-int reverse_digits(int n, int temp);
-bool checkPalindrome(int numeri);										   //5
-
-bool isPrime(int n, int i = 2);								  //4
-
-int sum_of_digit(int n);									 //3
-
-int product(int x, int y);									//2
-
-void insertionSortRecursive(int arr[], int n);			 //1
-
-std::string lookAndSay(int n);									 //7
-
-void myCopy(char s1[], char s2[], int index = 0);						//8
-
-bool isConsonant(char ch);
-int totalConsonants(std::string str, int n);								   //
-
-int getMax(int arr[], int n);
-int getMin(int arr[], int n);
-
-int sum(int n);
-
+void printArray(int arr[], int n);						 //Auxilary					
