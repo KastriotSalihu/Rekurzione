bf7352f9bcfdfbdeafae9666bbacda0cf8431cb5
Kastriot Salihu <49651929+KastriotSalihu@users.noreply.github.com>
2020-01-15 11:02:56 +0100

    Update RecurseHeader.h

--- RecurseHeader.h
+++ RecurseHeader.h
@@ -2,23 +2,25 @@
 #define newl std::cout<<std::endl;	//Prints a new line
 #define spac std::cout<<" "	    //Prints a space
 #define LOG() std::cout			 //Prints
+#define MAX 100                   //printA max number of elements
 
-int factorial_a(int n);                                               //Used	 
-void Numerim(int test);                                               //Used
-void display(int n);                                                  //Used
-void printPattern(int n, int m);                                      //Used			 
-int reverse_digits(int n, int temp);                                  //Used as Secondary to P
-bool checkPalindrome(int numeri);                                     //Used P
-bool isPrime(int n, int i = 2);                                       //Used
-int sum_of_digit(int n);                                              //Used
-int product(int x, int y);                                            //Used
-void insertionSortRecursive(int arr[], int n);                        //Used
-std::string lookAndSay(int n);                                        //Used
-bool isConsonant(char ch);                                            //Used as Secondary to J
-int removeSpaces(char* str, int n);                                   //Used as Secondary to J
-int totalConsonants(char *str, int n);                                //Used J
-int getMax(int arr[], int n);                                         //Used K
-int getMin(int arr[], int n);                                         //Used K
-int sum(int n);                                                       //Used 
-										
-void printArray(int arr[], int n);                       //Auxilary		
+int factorial_a(int);                                               //Used
+void Numerim(int);                                               //Used
+long int calculPow(int, int);                                     //Used
+void printPattern(int,int);                                      //Used
+int reverse_digits(int, int);                                  //Used as Secondary to P
+bool checkPalindrome(int);                                     //Used P
+bool isPrime(int, int = 2);                                       //Used
+int sum_of_digit(int);                                              //Used
+int product(int, int);                                            //Used
+void insertionSortRecursive(int arr[], int);                        //Used
+void printA(int array[],int,int);
+bool isConsonant(char);                                            //Used as Secondary to J
+int totalConsonants(std::string str, int n);                                //Used J
+int getMax(int arr[], int);                                         //Used K
+int getMin(int arr[], int);                                         //Used K
+int sum(int);                                                       //Used
+
+
+void printArray(int arr[], int);                       //Auxilary
+
