6594df81c7b614780a1421579171f0a929348274
Kastriot Salihu <49651929+KastriotSalihu@users.noreply.github.com>
2020-04-29 13:15:39 +0200

    Update RekurzioneFileMain.cpp

--- RekurzioneFileMain.cpp
+++ RekurzioneFileMain.cpp
@@ -6,73 +6,68 @@
 bool provoPalindrom() {
 	bool test;
 	int numeri;
-	newl;
-	std::cout << "Shtypni numerin qe deshironi te provoni a lexohet njejte edhe nga fundi: ";
+	std::cout << "\nShtypni numerin qe deshironi te provoni a lexohet njejte edhe nga fundi: ";
 	std::cin >> numeri;
 	test = checkPalindrome(numeri);
-	newl;
+	std::cout<< std::endl;
 	return test;
 }
 //Provon nese numri i dhene eshte i thjeshte
 bool provoNumerThjesht() {
 	bool test;
 	int numeri;
-	newl;
-	std::cout << "Shtypni numerin qe deshironi te provoni a eshte numer i thjeshte: ";
+	
+	std::cout << "\nShtypni numerin qe deshironi te provoni a eshte numer i thjeshte: ";
 	std::cin >> numeri;
 	test = isPrime(numeri);
 	if (test)	LOG() << "Po";
 	else		LOG() << "Jo";
-	newl;
+	std::cout<< std::endl;
 	return test;
 }
 //Gjen shumen e shifrave te nje numri
 void shumaEShifrave() {
 	int numeri;
-	newl;
-	std::cout << "Shtypni numerin shumen e shifrave te te cilit deshironi te gjeni: ";
+	std::cout << "\nShtypni numerin shumen e shifrave te te cilit deshironi te gjeni: ";
 	std::cin >> numeri;
 	int rezultati = sum_of_digit(numeri);
 	printf("Shuma e shifrave te numerit %d eshte %d\n", numeri, rezultati);
-	newl;
+	std::cout<< std::endl;
 }
 //Gjen prodhimin e dy numrave
 void prodhim() {
 	int x, y;
-	newl;
-	std::cout << "Shtypni numrat te cilet deshironi te shumezoni: " << std::endl
-		<< "x= ";
+	std::cout <<"\nShtypni numrat te cilet deshironi te shumezoni: " << std::endl
+		  << "x= ";
 	std::cin >> x;
 	std::cout << std::endl << "y= ";
 	std::cin >> y;
 	std::cout << "Prodhimi i numerit " << x << " dhe " << y << " eshte " << product(x, y);
-	newl;
+	std::cout<< std::endl;
 }
 //Shtyp nje varg numrash me ndryshim te rastesishem mes elementeve
 void seri() {
 	int n, m;
-	newl;
-	std::cout << "Shtypni numrin fillestar ne seri te numerave: ";
+	std::cout << "\nShtypni numrin fillestar ne seri te numerave: ";
 	std::cin >> n;
 	m = rand() % 10;
 	//printPattern(n, n, true);
 	printPattern(n, m+1);
-	newl;
+	std::cout<< std::endl;
 }
 //Numerim prej nje numri N ne ZERO ne N
 void numerimN0N() {
 	int numeri;
-	newl;
-	std::cout << "Shtypni numrin per fillim te numerimit: ";
+	std::cout << "\nShtypni numrin per fillim te numerimit: ";
 	std::cin >> numeri;
 	Numerim(numeri);
-	newl;
+	std::cout<< std::endl;
 }
 //Sorton nje varg te dhene me metoden Insertion Sort
 void renditVargun() {
 	const int ni = 5;
 	int arr[ni];
-	newl;
+	std::cout<< std::endl;
 	for (int i = 0; i < ni;i++) {
 		std::cout << "Elementi[" << i + 1 << "]= ";
 		std::cin >> arr[i];
@@ -83,51 +78,48 @@ void renditVargun() {
 	std::cout << "Vargu pas renditjes eshte: ";
 	insertionSortRecursive(arr, n);
 	printArray(arr, n);
-	newl;
+	std::cout<< std::endl;
 }
 //Shtyp varg me Rekurzion
 void ShtypVarg(){
     int arr[MAX];
     int p;
-    newl;
-    std::cout<<"Shtypni numrin e elementeve qe deshironi te"
-    <<" ruani ne varg: ";
+    std::cout << "\nShtypni numrin e elementeve qe deshironi te"
+              << " ruani ne varg: ";
     std::cin>>p;
     for(int i = 0;i<p;i++){
-        std::cout<<"Shtypni elementin "<<i+1<<": ";
+        std::cout<< "Shtypni elementin "<<i+1<<": ";
         std::cin>>arr[i];
     }
     printA(arr,0,p);
-    newl;
+    std::cout<< std::endl;
 }
 //Logarit fuqine e nje numri
 void bazeNeEksponent(){
     int x,y;
-    newl;
-std::cout<<"Shtypni vleren e bazes se fuqise: ";
-std::cin>>x;
-std::cout<<"Shtypni vleren e eksponentit: ";
-std::cin>>y;
-std::cout<<"Fuqia e kerkuar eshte: "<<calculPow(x,y);
-newl;
+    std::cout<< "\nShtypni vleren e bazes se fuqise: ";
+    std::cin >> x;
+    std::cout << "Shtypni vleren e eksponentit: ";
+    std::cin >> y;
+    std::cout << "Fuqia e kerkuar eshte: "<<calculPow(x,y);
+    std::cout<< std::endl;
 }
 //Shtyp numrin e bashketingelloreve ne nje fjali te dhene
 void NumriBashktinglloreve() {
 	std::string str;
-	newl;
-	std::cout << "Shtypni fjalen ne te cilen deshironi te numeroni bashketingelloret: ";
+	std::cout << "\nShtypni fjalen ne te cilen deshironi te numeroni bashketingelloret: ";
 	std::cin >> str;
 	std::cin.clear();
 	std::cin.ignore(INT_MAX,'\n');
 	std::cout << "Numri i bashketingelloreve ne fjalen \"" << str << "\" eshte ";
-    std::cout << totalConsonants(str, str.length());
-	newl;
+        std::cout << totalConsonants(str, str.length());
+	std::cout<< std::endl;
 }
 //Shtyp elementin minimal dhe maksimal ne nje varg te dhene
 void gjejMinDheMax() {
 	const int ni = 5;
 	int arry[ni];
-	newl;
+	std::cout<< std::endl;
 	for (int i = 0; i < ni;i++) {
 		std::cout << "Elementi[" << i + 1 << "]= ";
 		std::cin >> arry[i];
@@ -135,24 +127,22 @@ void gjejMinDheMax() {
 	int n = sizeof(arry) / sizeof(arry[0]);
 	std::cout << "Minimum element of array: " << getMin(arry, n) << "\n";
 	std::cout << "Maximum element of array: " << getMax(arry, n) << "\n";
-	newl;
+	std::cout<< std::endl;
 }
 //Shtyp faktorielin e nje numri n
 void faktorielIndirekt() {
 	int n;
-	newl;
-	std::cout << "Shtypni numrin per te gjetur faktorielin: ";
+	std::cout << "\nShtypni numrin per te gjetur faktorielin: ";
 	std::cin >> n;
 	std::cout << "Faktorieli i numrit " << n << " eshte " << factorial_a(n);
-	newl;
+	std::cout<< std::endl;
 }
 //Shtyp shumen e numrave natyror deri ne n
 void ShumeSigma() {
 	int n;
-	newl;
-	std::cout << "Shtypni numrin per te gjetur shumen: ";
+	std::cout << "\nShtypni numrin per te gjetur shumen: ";
 	std::cin >> n;
 	std::cout << "Shuma e numrave natyror deri tek numri i kerkuar " << n
 		<< " eshte " << sum(n) << std::endl;
-	newl;
+	std::cout<< std::endl;
 }
